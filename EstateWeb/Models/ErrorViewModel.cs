fc518f79cea01ac1f329300276b0429c1df6bf44
Lain <Lain@DESKTOP-7ND8AFH>
2024-07-10 01:21:42 +0330

    Add project files.

---
+++ EstateWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EstateWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
